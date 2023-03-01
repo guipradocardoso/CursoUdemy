15bc066bd5fe2fedce48e675f9138709ee0325d3
Guilherme Prado <guilhermeprado-09@hotmail.com>
2023-02-28 21:00:01 -0300

    Upcasting e Downcasting

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,39 @@
 package application;
 
+import entities.Account;
 import entities.BusinesAccount;
+import entities.SavingsAccount;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		BusinesAccount account = new BusinesAccount();
-				
-				
+		Account acc = new Account(1001, "Alex", 0.0);
+		BusinesAccount bacc = new BusinesAccount(1002, "Maria", 0.0, 500.0);
+
+		// UPCASTING
+
+		Account acc1 = bacc;
+		Account acc2 = new BusinesAccount(1003, "BOB", 0.0, 200.0);
+		Account acc3 = new SavingsAccount(1004, "Ana", 0.0, 0.01);
+
+		// DOWNCASTING
+
+		BusinesAccount acc4 = (BusinesAccount) acc2;
+		acc4.loan(100.0);
+
+		// BusinesAccount acc5 = (BusinesAccount) acc3;
+		if (acc3 instanceof BusinesAccount) {
+			BusinesAccount acc5 = (BusinesAccount) acc3;
+			acc5.loan(200.0);
+			System.out.println("Loan!");
+		}
+		if (acc3 instanceof SavingsAccount) {
+			SavingsAccount acc5 = (SavingsAccount) acc3;
+			acc5.UpdatedBalance();
+			System.out.println("Updated!");
+		}
+
 	}
 
 }
